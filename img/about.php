8ee322fc43fd92751b08e309ec4a27b5d65713dc
obisopulu <obisopulu@gmail.com>
2022-01-04 22:03:50 +0100

    first commit

---
+++ img/about.php
@@ -0,0 +1,107 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dominion Group of Schools</title>
+<link type="text/css" rel="stylesheet" href="cascader_dgs.css" />
+</head>
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0"> <tr class="header">
+  	<td>&nbsp;</td>
+    <td><img width="100px" height="100px" src="img/logo_dgs.jpg"</td>
+  	<td>&nbsp;</td>
+</tr><tr class="nav">
+  	<td>&nbsp;</td>  
+    <td><table border="0" width="100%"><tr>
+	    <td><a href="#">about us</a></td>
+		<td><a href="#">insulation products</a></td>        
+	    <td><a href="#">installation</a></td>
+		<td><a href="#">blog</a></td>        
+	    <td><a href="#">contacts</a></td>
+    </tr></table></td>
+  	<td>&nbsp;</td>    
+</tr>
+<!--<tr class="wall">
+  	<td>&nbsp;</td>  
+    <td>
+    <table border="0" width="100%"><tr>
+	    <td><a href="#" class="map"><h1>HISTORY<h1></a></td>
+		<td><a href="#" class="map"><h2>ADMINISTRATIVE HIERACHY</h2></a></td>
+    </tr></table>
+    </td>
+  	<td>&nbsp;</td>    
+</tr>-->
+<tr class="">
+  	<td>&nbsp;</td>  
+    <td>
+    <table border="0" width="100%"><tr>
+		<td class="history">
+        <h1>MOTTO</h1>
+        </td>
+        <td class="pad20">
+<b>Nursery/Primary:</b>Knowledge, wisdom and fear of God.<br/>
+<b>Secondary School</b>Building future leaders.<br/>
+Mission Statement: our target is to build christian leaders in the knowledge, wisdom and fear of God.
+
+        </td>
+    </tr></table>
+    </td>
+  	<td>&nbsp;</td>    
+</tr>
+<tr class="">
+  	<td>&nbsp;</td>  
+    <td>
+    <table border="0" width="100%"><tr>
+		<td class="history">
+        <h1>HISTORY OF DGS</h1>
+        </td>
+        <td class="pad20">
+Dominion Group of Schools made up of a Daycare, Nursery, Primary and Secondary School was founded srptember 2002, while the Secondary School began sep. 2012 at  the permanent site.<br/>
+
+        </td>
+    </tr></table>
+    </td>
+  	<td>&nbsp;</td>    
+</tr>
+
+<tr class="">
+  	<td>&nbsp;</td>  
+    <td>
+    <table border="0" width="100%"><tr>
+		<td class="history">
+        <h1>ADMINISTRATIVE HIERACHY</h1>
+        </td>
+        <td class='pad20'>
+<b>Chairman:</b> Bishop Kenneth U. Ebolum. He is the Chairman and one of the founding members of this institution with the aim of promoting confidence and building the next generation of future leaders and he is the General Overseer of the El-Elyon Gospel Ministries Worldwide with headquarters Dominion Christian Center Delta State.<br/>
+<b>Proprietress:</b> Rev. Mrs Esther Ebolum. She is the Proprietress and Administrative Head of DGS Asaba. She is the wife of the Chairman and National Pastor of DCC.
+Principal/Head of School<br/>
+Headmistress<br/>
+Administrative Staff:<br/>
+Accountants<br/>
+Teachers<br/>
+Daycare attendants<br/>
+Ict Departnent<br/>
+Drivers<br/>
+Cleaners.<br/>
+
+        </td>
+    </tr></table>
+    </td>
+  	<td>&nbsp;</td>    
+</tr>
+
+<tr class="footer">
+  	<td>&nbsp;</td>  
+    <td>
+    &copy; 2015 <a>HWPLabs</a>, all rights reserved.
+    <span id="footLinks">
+    <a href="#" id="sel">facebook</a>
+    <a href="#">twitter</a>
+    <a href="#">google+</a>            
+    </span>
+    </td>
+  	<td>&nbsp;</td>    
+  </tr>
+</table>
+</body>
+</html>
---
+++ img/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dominion Group of Schools</title>
+<link type="text/css" rel="stylesheet" href="cascade_dgs.css" />
+</script>
+</head>
+<body onLoad="clearInp()">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr class="header">
+  	<td>&nbsp;</td>
+    <td><img width="100px" height="100px" src="img/logo_dgs.jpg"</td>
+  	<td>&nbsp;</td>
+</tr>
+  <tr class="nav">
+  	<td>&nbsp;</td>  
+    <td>
+    <table border="0" width="100%">
+    <tr>
+	    <td><a href="index.php">Home</a></td>
+		<td><a href="about.php">About</a></td>        
+	    <td><a href="#">Admissions</a></td>
+		<td><a href="#">Calender</a></td>        
+	    <td><a href="#">Gallery</a></td>
+    </tr>
+    </table>
+    </td>
+  	<td>&nbsp;</td>    
+  </tr>
+  <tr class="wall">
+  	<td>&nbsp;</td>  
+    <td>
+        <h1>make your home<br/>warmer and healthier</h1>
+        <p></p>
+		<h5>Examples might be simplified to improve reading and basic understanding.
+        <br/>Tutorials, references, and examples are constantly reviewed to avoid.
+        <br/>W3Schools is optimized for learning, testing, and training.</h5>
+        <p></p>        
+        <a href="#" id="button">more info</a>
+        <span class="pagi">
+        <a href="#">&nbsp;</a>
+        <a href="#">&nbsp;</a>        
+        <a href="#" id="sel">&nbsp;</a>        
+        </span>
+    </td>
+  	<td>&nbsp;</td>    
+  </tr>
+  <tr class="article">
+  	<td>&nbsp;</td>  
+    <td>
+		<table border="0">
+        <tr>
+	        <td rowspan="2" width="50%">
+            	<span id="art_tit">welcome</span><br/>
+                <span id="art_sub">to our company site<p></p></span>
+                <h3>W3Schools is optimized for discovering</h3>     
+                <h5>Examples might be simplified to improve reading and basic 
+                understanding. Tutorials, references, and examples are constantly 
+                reviewed to avoid errors, but we cannot warrent full correctnes of 
+                all content.</h5>
+            </td>
+	        <td colspan="2">
+            <h2>testimonials</h2>      
+            <a href="#" id="sel">></a>            
+            <a href="#"><</a>
+            <p></p>
+            </td>            
+        </tr>
+        <tr>
+	        <td>
+            <img src="img/pro_1.jpeg" />
+            <h3 id="name">emma Emmanuel</h3>
+            <h5>Examples might be simplified to improve reading and basic 
+            understanding. Tutorials, references, and examples are constantly 
+            reviewed to avoid correctnes of all content.</h5>
+            </td>
+	        <td>
+            <img src="img/pro_2.jpg" />
+            <h3 id="name">dave ghrol</h3>
+            <h5>Examples might be simplified to improve reading and basic 
+            understanding. Tutorials, references, and examples are constantly 
+            reviewed to avoid full correctnes of all content.</h5>
+            </td>
+        </tr>
+        </table>
+    </td>
+  	<td>&nbsp;</td>    
+  </tr>
+  <tr class="sec1">
+  	<td>&nbsp;</td>  
+    <td>
+        <table border="0">
+        <tr>
+            <td><h2>about home insulation</h2></td>
+            <td><h2>latest news</h2></td>
+        </tr>
+        <tr>    
+            <td>
+            <img src="img/sec1_1.png" />
+            <h3>W3Schools is optimized for learning.</h3>
+            <h5>Examples might be simplified to improve reading and basic 
+            understanding tutorials, references, and examples.
+            <p></p> 
+            Examples might be simplified to improve reading and basic 
+            understanding tutorials, references, and examples. Are constantly 
+            reviewed to avoid errors, but we cannot warrent full correctnes content.</h5>    
+            </td>
+            <td>
+            <div class="badge">
+	            <h3>01</h3>
+                <h5>jan</h5>
+            </div>
+            <h3>donec facilisi ulet</h3>
+            <h4>lorem ipsum dolore</h4>
+            <h5>Examples might be simplified to. improve reading understanding.</h5>
+            <p></p>
+            <div class="badge">
+	            <h3>15</h3>
+                <h5>sep</h5>
+            </div>
+            <h3>donec facilisi ulet</h3>
+            <h4>lorem ipsum dolore</h4>
+            <h5>Tutorials, references, and examples are constantly reviewed to avoid.</h5>    
+            </td>
+        </tr>
+        </table>
+    </td>
+  	<td>&nbsp;</td>    
+  </tr>
+  <tr class="sec2">
+  	<td>&nbsp;</td>  
+    <td>
+    <table border="0">
+      <tr>
+        <td colspan="2"><h2>types of insulation</h2></td>
+        <td><h2>select a category</h2></td>
+      </tr>
+      <tr>
+        <td>
+        	<h3>pink insulation</h3>
+            <h4>vestibulum port mauris.</h4>
+            <h5>Tutorials, references, and examples are constantly reviewed to avoid 
+            errors, but we cannot warrent full correctnes of all content.</h5>
+            <p></p>            
+	        <a href="#" id="button">more info</a>
+        </td>
+        <td>
+        	<h3>cellulose sprays</h3>
+            <h4>vestibulum port mauris.</h4>
+            <h5>Tutorials, references, and examples are constantly reviewed to avoid 
+            errors, but we cannot warrent full correctnes of all content.</h5>
+            <p></p>            
+    	    <a href="#" id="button">more info</a>
+        </td>
+        <td rowspan="2">
+        <ul>
+            <li>speedline</li>
+            <li>Highscore</li>
+            <li>Color</li>
+            <li>Magic Media</li>
+            <li>BerniceXMichelle, BxM</li>
+            <li>Cheeze Online Magazine</li>
+            <li>Elm-E designs</li>
+            <li>HWP Pronto group chat</li>
+            <li>God is Good motors tour</li>
+            <li>HWP SmallTalk</li>
+            <li>Wanabet.com by DexaCorp</li>
+            <li>GSI website</li>
+            <li>OWMS</li>
+            <li>FHA Mortgage Bank Ltd.</li>
+            <li>industrial living</li>
+        </ul>
+        </td>
+      </tr>
+      <tr>
+        <td>
+        	<h3>foam sprays</h3>
+            <h4>vestibulum port mauris.</h4>
+            <h5>Tutorials, references, and examples are constantly reviewed to avoid 
+            errors, but we cannot warrent full correctnes of all content.</h5>
+            <p></p>
+	        <a href="#" id="button">more info</a>
+        </td>
+        <td>
+        	<h3>reflective insulation</h3>
+            <h4>vestibulum port mauris.</h4>
+            <h5>Tutorials, references, and examples are constantly reviewed to avoid 
+            errors, but we cannot warrent full correctnes of all content.</h5>
+            <p></p>
+            <a href="#" id="button">more info</a>
+        </td>
+      </tr>
+    </table>
+  	<td>&nbsp;</td>    
+    </td>
+  </tr>
+  <tr class="footer">
+  	<td>&nbsp;</td>  
+    <td>
+    &copy; 2015 <a>HWPLabs</a>, all rights reserved.
+    <span id="footLinks">
+    <a href="#" id="sel">facebook</a>
+    <a href="#">twitter</a>
+    <a href="#">google+</a>            
+    </span>
+    </td>
+  	<td>&nbsp;</td>    
+  </tr>
+</table>
+</body>
+</html>
